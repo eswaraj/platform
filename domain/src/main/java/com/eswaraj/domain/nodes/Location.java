265357943ed9eb386f9752e93006581b58e41561
Ravi Sharma <ping2ravi@gmail.com>
2014-07-25 04:48:25 +0100

    PLAT-27 Added Moe fields into Location
    Total Number of Houses 
    Total Population 
    Total Male Population 
    Total Female Population 
    Total Literate Population 
    Total Male Literate Population 
    Total Female Literate Population 
    Total Working Population 
    Total Male Working Population 
    Total FeMale Working Population

--- domain/src/main/java/com/eswaraj/domain/nodes/Location.java
+++ domain/src/main/java/com/eswaraj/domain/nodes/Location.java
@@ -27,7 +27,27 @@ public class Location extends BaseNode {
 	private Double latitude;
 	
 	private Double longitude;
-
+	
+	private Long totalNumberOfHouses;
+	
+	private Long totalPopulation;
+	
+	private Long totalMalePopulation;
+	
+	private Long totalFemalePopulation;
+	
+	private Long totalLiteratePopulation;
+	
+	private Long totalMaleLiteratePopulation;
+	
+	private Long totalFemaleLiteratePopulation;
+	
+	private Long totalWorkingPopulation;
+	
+	private Long totalMaleWorkingPopulation;
+	
+	private Long totalFemaleWorkingPopulation;
+	
 	public String getName() {
 		return name;
 	}
@@ -66,6 +86,86 @@ public class Location extends BaseNode {
 
 	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
+	}
+
+	public Long getTotalNumberOfHouses() {
+		return totalNumberOfHouses;
+	}
+
+	public void setTotalNumberOfHouses(Long totalNumberOfHouses) {
+		this.totalNumberOfHouses = totalNumberOfHouses;
+	}
+
+	public Long getTotalPopulation() {
+		return totalPopulation;
+	}
+
+	public void setTotalPopulation(Long totalPopulation) {
+		this.totalPopulation = totalPopulation;
+	}
+
+	public Long getTotalMalePopulation() {
+		return totalMalePopulation;
+	}
+
+	public void setTotalMalePopulation(Long totalMalePopulation) {
+		this.totalMalePopulation = totalMalePopulation;
+	}
+
+	public Long getTotalFemalePopulation() {
+		return totalFemalePopulation;
+	}
+
+	public void setTotalFemalePopulation(Long totalFemalePopulation) {
+		this.totalFemalePopulation = totalFemalePopulation;
+	}
+
+	public Long getTotalLiteratePopulation() {
+		return totalLiteratePopulation;
+	}
+
+	public void setTotalLiteratePopulation(Long totalLiteratePopulation) {
+		this.totalLiteratePopulation = totalLiteratePopulation;
+	}
+
+	public Long getTotalMaleLiteratePopulation() {
+		return totalMaleLiteratePopulation;
+	}
+
+	public void setTotalMaleLiteratePopulation(Long totalMaleLiteratePopulation) {
+		this.totalMaleLiteratePopulation = totalMaleLiteratePopulation;
+	}
+
+	public Long getTotalFemaleLiteratePopulation() {
+		return totalFemaleLiteratePopulation;
+	}
+
+	public void setTotalFemaleLiteratePopulation(Long totalFemaleLiteratePopulation) {
+		this.totalFemaleLiteratePopulation = totalFemaleLiteratePopulation;
+	}
+
+	public Long getTotalWorkingPopulation() {
+		return totalWorkingPopulation;
+	}
+
+	public void setTotalWorkingPopulation(Long totalWorkingPopulation) {
+		this.totalWorkingPopulation = totalWorkingPopulation;
+	}
+
+	public Long getTotalMaleWorkingPopulation() {
+		return totalMaleWorkingPopulation;
+	}
+
+	public void setTotalMaleWorkingPopulation(Long totalMaleWorkingPopulation) {
+		this.totalMaleWorkingPopulation = totalMaleWorkingPopulation;
+	}
+
+	public Long getTotalFemaleWorkingPopulation() {
+		return totalFemaleWorkingPopulation;
+	}
+
+	public void setTotalFemaleWorkingPopulation(Long totalFemaleWorkingPopulation) {
+		this.totalFemaleWorkingPopulation = totalFemaleWorkingPopulation;
 	}
 
 	@Override
