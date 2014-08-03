c9896e8e8fe29d2aa6d77eecb96091e34f9951bd
Ravi Sharma <ping2ravi@gmail.com>
2014-08-03 14:18:00 +0100

    More logs

--- tasks/src/main/java/com/eswaraj/tasks/spout/LocationFileUploadSpout.java
+++ tasks/src/main/java/com/eswaraj/tasks/spout/LocationFileUploadSpout.java
@@ -19,8 +19,11 @@ public class LocationFileUploadSpout extends EswarajBaseSpout {
             if (message != null) {
                 logInfo("Mesage Recieved in Spout :  " + message);
                 writeToStream(new Values(message));
+                logInfo("Mesage Emitted from :  " + message);
             }
         } catch (ApplicationException e) {
+            logError("Unable to receive Location File message from AWS Quque", e);
+        } catch (Exception e) {
             logError("Unable to receive Location File message from AWS Quque", e);
         }
     }
