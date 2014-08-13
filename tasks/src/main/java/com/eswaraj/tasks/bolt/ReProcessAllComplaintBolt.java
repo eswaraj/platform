d4e7296e9a459d4984895ad21de8f75be870ba3b
Ravi Sharma <ping2ravi@gmail.com>
2014-08-13 05:18:04 +0100

    removed Spring context from a bolt which wasnt using it

--- tasks/src/main/java/com/eswaraj/tasks/bolt/ReProcessAllComplaintBolt.java
+++ tasks/src/main/java/com/eswaraj/tasks/bolt/ReProcessAllComplaintBolt.java
@@ -1,17 +1,12 @@
 package com.eswaraj.tasks.bolt;
 
 import java.util.Iterator;
-import java.util.Map;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.neo4j.conversion.EndResult;
 
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
-import com.eswaraj.core.service.ComplaintService;
 import com.eswaraj.domain.nodes.Complaint;
 import com.eswaraj.tasks.topology.EswarajBaseBolt;
 
@@ -21,18 +16,6 @@ public class ReProcessAllComplaintBolt extends EswarajBaseBolt {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-    private ComplaintService complaintService;
-    private ClassPathXmlApplicationContext applicationContext;
-
-    @Override
-    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
-        super.prepare(stormConf, context, collector);
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("task-spring-context.xml");
-        complaintService = applicationContext.getBean(ComplaintService.class);
-    }
-
-
 
 	@Override
     public Result processTuple(Tuple input) {
@@ -52,15 +35,5 @@ public class ReProcessAllComplaintBolt extends EswarajBaseBolt {
         return Result.Failed;
 	}
 
-    @Override
-    public void cleanup() {
-        super.cleanup();
-        try {
-            applicationContext.close();
-        } catch (Exception ex) {
-            logError("Unable to close application context", ex);
-        }
-
-    }
 
 }
