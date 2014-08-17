aa0352d9610f1ccd7e073852501cbacc642e0bfa
Ravi Sharma <ping2ravi@gmail.com>
2014-08-17 08:01:40 +0100

    Plat-42 , API to get last 30 days Complaints count of a location

--- web/src/main/java/com/eswaraj/web/admin/controller/ApiController.java
+++ web/src/main/java/com/eswaraj/web/admin/controller/ApiController.java
@@ -1,5 +1,8 @@
 package com.eswaraj.web.admin.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eswaraj.core.exceptions.ApplicationException;
+import com.eswaraj.core.service.CounterKeyService;
 import com.eswaraj.core.service.LocationKeyService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 @Controller
 public class ApiController {
@@ -20,6 +26,8 @@ public class ApiController {
     private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private LocationKeyService locationKeyService;
+    @Autowired
+    private CounterKeyService counterKeyService;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -30,5 +38,33 @@ public class ApiController {
         logger.info("getting data from Redis for key {}", redisKeyForLocationInfo);
         String data = stringRedisTemplate.opsForValue().get(redisKeyForLocationInfo);
         return data;
+    }
+
+    @RequestMapping(value = "/api/location/{locationId}/complaintcounts/last30", method = RequestMethod.GET)
+    @ResponseBody
+    public String getLocationComplaintCountForLast30Days(ModelAndView mv, @PathVariable Long locationId) throws ApplicationException {
+        String keyPrefix = counterKeyService.getLocationKeyPrefix(locationId);
+        List<String> redisKeyForLocation30DaysCounter = counterKeyService.getHourComplaintKeysForLast30Days(keyPrefix, new Date());
+        logger.info("getting data from Redis for keys {}", redisKeyForLocation30DaysCounter);
+        List<String> data = stringRedisTemplate.opsForValue().multiGet(redisKeyForLocation30DaysCounter);
+        Long totalComplaints = 0L;
+        int count = 0;
+        JsonArray jsonArray = new JsonArray();
+        JsonObject jsonObject;
+        for (String oneString : data) {
+            jsonObject = new JsonObject();
+            if (oneString != null) {
+                jsonObject.addProperty(redisKeyForLocation30DaysCounter.get(count).replace(keyPrefix, ""), oneString);
+                totalComplaints = totalComplaints + Long.parseLong(oneString);
+            } else {
+                jsonObject.addProperty(redisKeyForLocation30DaysCounter.get(count).replace(keyPrefix, ""), "0");
+            }
+            jsonArray.add(jsonObject);
+            count++;
+        }
+        JsonObject returnObject = new JsonObject();
+        returnObject.addProperty("totalComplaints", totalComplaints);
+        returnObject.add("dayWise", jsonArray);
+        return returnObject.toString();
     }
 }
