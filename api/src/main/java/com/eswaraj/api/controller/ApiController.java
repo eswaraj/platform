9adc36887da419c865c80d867a0d5f527774bd5c
Ravi Sharma <ping2ravi@gmail.com>
2014-08-25 10:45:17 +0100

    More apis

--- api/src/main/java/com/eswaraj/api/controller/ApiController.java
+++ api/src/main/java/com/eswaraj/api/controller/ApiController.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.eswaraj.core.exceptions.ApplicationException;
 import com.eswaraj.core.service.AppKeyService;
+import com.eswaraj.core.service.CounterKeyService;
 import com.eswaraj.core.service.LocationKeyService;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -36,9 +37,8 @@ public class ApiController extends BaseController {
     private AppKeyService appKeyService;
     @Autowired
     private RedisUtil redisUtil;
-    /*
-     * @Autowired private CounterKeyService counterKeyService;
-     */
+    @Autowired
+    private CounterKeyService counterKeyService;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/api/v0/location/{locationId}/info", method = RequestMethod.GET)
@@ -50,7 +50,6 @@ public class ApiController extends BaseController {
         List<Map> results = redisUtil.executeAll();
         return (String) results.get(0).get(locationId);
     }
-    /*
     @RequestMapping(value = "/api/v0/location/{locationId}/complaintcounts/last30", method = RequestMethod.GET)
     @ResponseBody
     public String getLocationComplaintCountForLast30Days(ModelAndView mv, @PathVariable Long locationId) throws ApplicationException {
@@ -133,5 +132,4 @@ public class ApiController extends BaseController {
         }
         return Integer.parseInt(paramValue);
     }
-    */
 }
