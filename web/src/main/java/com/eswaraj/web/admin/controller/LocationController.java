14cefb1f9c010f18b0d3bf8374cfe9903d3a1919
Ravi Sharma <ping2ravi@gmail.com>
2014-09-04 05:09:35 +0100

    added location Controler + url Identifiers

--- web/src/main/java/com/eswaraj/web/admin/controller/LocationController.java
+++ web/src/main/java/com/eswaraj/web/admin/controller/LocationController.java
@@ -1,5 +1,7 @@
 package com.eswaraj.web.admin.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,14 @@ public class LocationController extends BaseController {
 
     @RequestMapping(value = "/const.html", method = RequestMethod.GET)
     public ModelAndView showIndexPage(ModelAndView mv) {
+        addGenericValues(mv);
+        mv.setViewName("constituency");
+        return mv;
+    }
+
+    @RequestMapping(value = "/state/*", method = RequestMethod.GET)
+    public ModelAndView showLocationPage(ModelAndView mv, HttpServletRequest httpServletRequest) {
+        System.out.println("Request URI : " + httpServletRequest.getRequestURI());
         addGenericValues(mv);
         mv.setViewName("constituency");
         return mv;
