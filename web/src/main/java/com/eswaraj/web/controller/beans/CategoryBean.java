2c2e14abd624bfd539d0c46442ba9155015b3ff9
Ravi Sharma <ping2ravi@gmail.com>
2014-09-07 21:06:38 +0100

    another bean fix

--- web/src/main/java/com/eswaraj/web/controller/beans/CategoryBean.java
+++ web/src/main/java/com/eswaraj/web/controller/beans/CategoryBean.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class CategoryBean {
 
+    private Long id;
     private String externalId;
     private String headerImageurl;
     private String imageUrl;
@@ -13,6 +14,14 @@ public class CategoryBean {
     private Long globalCount;
     private boolean root;
     private List<CategoryBean> childCategories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getExternalId() {
         return externalId;
