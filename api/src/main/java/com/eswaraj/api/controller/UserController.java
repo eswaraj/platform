d35898fe6700d7a25d187800b997e476afd01e67
Ravi Sharma <ping2ravi@gmail.com>
2014-09-14 03:19:14 +0100

    Added annotation

--- api/src/main/java/com/eswaraj/api/controller/UserController.java
+++ api/src/main/java/com/eswaraj/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.eswaraj.api.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.eswaraj.web.dto.RegisterFacebookAccountRequest;
 import com.eswaraj.web.dto.RegisterFacebookAccountWebRequest;
 import com.eswaraj.web.dto.UserDto;
 
+@Controller
 public class UserController extends BaseController {
 
     @Autowired
