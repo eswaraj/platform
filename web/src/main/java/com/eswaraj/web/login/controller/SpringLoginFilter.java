cb4970fcf4c18738b12c0d44e19f5419b103a1fb
Ravi Sharma <ping2ravi@gmail.com>
2014-09-14 13:54:56 +0100

    Fixing Filters

--- web/src/main/java/com/eswaraj/web/login/controller/SpringLoginFilter.java
+++ web/src/main/java/com/eswaraj/web/login/controller/SpringLoginFilter.java
@@ -2,8 +2,12 @@ package com.eswaraj.web.login.controller;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,22 +15,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class SpringLoginFilter extends OncePerRequestFilter {
+public class SpringLoginFilter implements Filter {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private SessionUtil sessionUtil;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (sessionUtil.getLoggedInUserFromSession(request) == null) {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        if (sessionUtil.getLoggedInUserFromSession(httpServletRequest) == null) {
             // No user logegd In
-            String redirectUrl = "/web/login/facebook?redirect_url=" + request.getRequestURI();
+            String redirectUrl = "/web/login/facebook?redirect_url=" + httpServletRequest.getRequestURI();
             logger.info("User Not logged In Redirecting to {}", redirectUrl);
-            response.sendRedirect("/web/login/facebook?redirect_url=" + redirectUrl);
+            ((HttpServletResponse) response).sendRedirect("/web/login/facebook?redirect_url=" + redirectUrl);
         }
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
 
     }
 
