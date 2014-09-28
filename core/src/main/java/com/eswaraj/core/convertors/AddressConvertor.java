199f2a4e8c1d3f3986b3876b7bcfefd675209bec
Ravi Sharma <ping2ravi@gmail.com>
2014-09-28 12:06:54 +0100

    Fixing null pointer exception

--- core/src/main/java/com/eswaraj/core/convertors/AddressConvertor.java
+++ core/src/main/java/com/eswaraj/core/convertors/AddressConvertor.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Component;
 import com.eswaraj.core.exceptions.ApplicationException;
 import com.eswaraj.domain.nodes.Address;
 import com.eswaraj.domain.nodes.Location;
+import com.eswaraj.domain.nodes.LocationType;
 import com.eswaraj.domain.repo.AddressRepository;
 import com.eswaraj.domain.repo.LocationRepository;
+import com.eswaraj.domain.repo.LocationTypeRepository;
 import com.eswaraj.web.dto.AddressDto;
 
 @Component
@@ -24,6 +26,8 @@ public class AddressConvertor extends BaseConvertor<Address, AddressDto> {
 	private LocationRepository locationRepository; 
     @Autowired
     private LocationConvertor locationConvertor;
+    @Autowired
+    private LocationTypeRepository locationTypeRepository;
 
 	
 	@Override
@@ -61,23 +65,27 @@ public class AddressConvertor extends BaseConvertor<Address, AddressDto> {
 		BeanUtils.copyProperties(dbDto, addressDto);
         if (dbDto.getLocations() != null && !dbDto.getLocations().isEmpty()) {
             // Flaten all Locations
+            LocationType locationType;
+            Location location;
             for (Location oneLocation : dbDto.getLocations()) {
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("Country")) {
+                // location = locationRepository.findOne(oneLocation.getId());
+                locationType = locationTypeRepository.findOne(oneLocation.getLocationType().getId());
+                if (locationType.getName().equalsIgnoreCase("Country")) {
                     addressDto.setCountry(locationConvertor.convertBean(oneLocation));
                 }
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("State")) {
+                if (locationType.getName().equalsIgnoreCase("State")) {
                     addressDto.setState(locationConvertor.convertBean(oneLocation));
                 }
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("District")) {
+                if (locationType.getName().equalsIgnoreCase("District")) {
                     addressDto.setDistrict(locationConvertor.convertBean(oneLocation));
                 }
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("Assembly Constituency")) {
+                if (locationType.getName().equalsIgnoreCase("Assembly Constituency")) {
                     addressDto.setAc(locationConvertor.convertBean(oneLocation));
                 }
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("Parliament Constituency")) {
+                if (locationType.getName().equalsIgnoreCase("Parliament Constituency")) {
                     addressDto.setPc(locationConvertor.convertBean(oneLocation));
                 }
-                if (oneLocation.getLocationType().getName().equalsIgnoreCase("Ward")) {
+                if (locationType.getName().equalsIgnoreCase("Ward")) {
                     addressDto.setWard(locationConvertor.convertBean(oneLocation));
                 }
                 System.out.println("oneLocation : " + oneLocation);
