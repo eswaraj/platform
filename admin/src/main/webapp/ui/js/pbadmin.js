326fd99be367751c60a28a466549c8cf103bf657
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-03 02:41:49 +0530

    Edit text

--- admin/src/main/webapp/ui/js/pbadmin.js
+++ admin/src/main/webapp/ui/js/pbadmin.js
@@ -476,7 +476,7 @@ function get_pbadmin(locationId,pbtypeId){
                     async: false,
                     success: function(d){
                         if(d){
-                            $('#pbadmin_list_current').append('<p>'+d.name+'</p><input type="button" class="admin_edit" onclick="showEdit(event)" location_id="'+locationId+'" pbtype_id="'+pbtypeId+'" value="Edit">Edit</input>');
+                            $('#pbadmin_list_current').append('<p>'+d.name+'</p><input type="button" class="admin_edit" onclick="showEdit(event);" location_id="'+locationId+'" pbtype_id="'+pbtypeId+'" value="Edit"></input>');
                         }
                     }
                 });
