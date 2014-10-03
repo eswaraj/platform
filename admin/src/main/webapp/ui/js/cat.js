364ca9cdf4c1a4914b2fcf6783fcfbbd95257dce
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-04 01:07:23 +0530

    jstree fix

--- admin/src/main/webapp/ui/js/cat.js
+++ admin/src/main/webapp/ui/js/cat.js
@@ -5,6 +5,7 @@ $(function(){
 $(document).ready(function(){
 
 var root_node;
+window.hash = new Object();
 
 $.ajax({
   type: "GET",
@@ -55,14 +56,17 @@ $.ajax({
 		
 		$('#cat_videoUrl').val($('#'+parent).attr('videoUrl'));
 		
-		if(!$('#'+parent).hasClass('jstree-open')){
-		var new_node = {'text':'fake','id':'fake_node'};
-		$('#js_tree').jstree(true).create_node(parent, new_node);
+		if(!($('#'+parent).hasClass('jstree-open')) && !window.hash.hasOwnProperty('fake_node'+$('#'+parent).attr('id'))){
+			new_node = {'text':'fake','id':'fake_node'+$('#'+parent).attr('id')};
+			$('#js_tree').jstree(true).create_node(parent, new_node);
+			window.hash['fake_node'+$('#'+parent).attr('id')] = 1;
 		}
     }).bind("open_node.jstree",function(e,data){
 	
 	//alert(JSON.stringify(data.node.id));	
-	$("#js_tree").jstree("delete_node", $('#fake_node'));
+	var parent = data.node.id;
+	$("#js_tree").jstree("delete_node", $('#fake_node'+$('#'+parent).attr('id')));
+			if($('#'+parent).closest("li").children("ul").length ==0){
 	var selected_node =  data.node.id;
 	var new_node;
 	var sel;
@@ -91,6 +95,7 @@ $.ajax({
 
 	
 	//$("#js_tree").jstree("open_node", data.node.id);
+			}
 	});    
 	
    $('#js_tree').jstree("select_node",root_node_array[0].id);	
