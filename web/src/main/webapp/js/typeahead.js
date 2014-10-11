edc3b24be5bbcdd783ba82dd0de45fadf089e233
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-11 16:12:40 +0530

    Fix

--- web/src/main/webapp/js/typeahead.js
+++ web/src/main/webapp/js/typeahead.js
@@ -25,7 +25,7 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
                 }
             });
             scope.handleSelection=function(selectedItem){
-                scope.model=selectedItem[scope.title];
+                scope.model=selectedItem;
                 scope.current=0;
                 scope.selected=true;        
                 $timeout(function(){
@@ -42,7 +42,7 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
             };
         },
         //templateUrl: 'templates/typeahead.html'
-	template : '<input type="text" ng-model="model" placeholder="{{prompt}}" ng-keydown="selected=false"/><br/>\
+	template : '<input type="text" ng-model="model[title]" placeholder="{{prompt}}" ng-keydown="selected=false"/><br/>\
 			<div class="items" ng-hide="!model.length || selected">\
 				<div class="item" ng-repeat="item in items | filter:model  track by $index" ng-click="handleSelection(item)" style="cursor:pointer" ng-class="{active:isCurrent($index)}" ng-mouseenter="setCurrent($index)">\
 				    <img class="image" src="{{item[img]}}" ng-show="item[img]"></img>\
