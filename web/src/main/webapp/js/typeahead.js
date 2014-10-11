ffcdbe4398c7972ff845d039db68db624ce3e9a7
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-11 15:01:37 +0530

    Fixes

--- web/src/main/webapp/js/typeahead.js
+++ web/src/main/webapp/js/typeahead.js
@@ -43,7 +43,7 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
         //templateUrl: 'templates/typeahead.html'
 	template : '<input type="text" ng-model="model" placeholder="{{prompt}}" ng-keydown="selected=false"/><br/>\
 			<div class="items" ng-hide="!model.length || selected">\
-				<div class="item" ng-repeat="item in items | filter:model  track by $index" ng-click="handleSelection(item[title])" style="cursor:pointer" ng-class="{active:isCurrent($index)}" ng-mouseenter="setCurrent($index)">\
+				<div class="item" ng-repeat="item in items | filter:model  track by $index" ng-click="handleSelection(item)" style="cursor:pointer" ng-class="{active:isCurrent($index)}" ng-mouseenter="setCurrent($index)">\
 				    <img class="image" src="{{item[img]}}" ng-show="item[img]"></img>\
 				<p class="title">{{item[title]}}</p>\
 				<p class="subtitle">{{item[subtitle]}}</p>\
