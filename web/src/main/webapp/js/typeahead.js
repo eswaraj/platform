139072420d8177ac5d14b2d5fc3834db79149721
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-11 19:19:46 +0530

    Try

--- web/src/main/webapp/js/typeahead.js
+++ web/src/main/webapp/js/typeahead.js
@@ -5,7 +5,6 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
         restrict: 'AEC',
         scope: {
             items: '=',
-	    text: '=',
             prompt:'@',
             title: '@',
             subtitle:'@',
@@ -18,17 +17,16 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
         },
         link:function(scope,elem,attrs){
 	    scope.model = scope.model || {};
-	    scope.text = scope.text || "";
-            scope.$watch('text', function () {
-                if(scope.text.length >= scope.min && !scope.selected) {
-                    dataFactory.get(scope.url, scope.text, scope.querystring).then(function(resp){
+	    scope.model[scope.title] = scope.model[scope.title] || "";
+            scope.$watch('model[title]', function () {
+                if(scope.model[scope.title].length >= scope.min && !scope.selected) {
+                    dataFactory.get(scope.url, scope.model[scope.title], scope.querystring).then(function(resp){
                         scope.items=resp.data;
                     });
                 }
             });
             scope.handleSelection=function(selectedItem){
                 scope.model=selectedItem;
-		scope.text = selectedItem[scope.title];
                 scope.current=0;
                 scope.selected=true;        
                 $timeout(function(){
@@ -45,8 +43,8 @@ typeAhead.directive('typeahead', function($timeout, dataFactory) {
             };
         },
         //templateUrl: 'templates/typeahead.html'
-	template : '<input type="text" ng-model="text" placeholder="{{prompt}}" ng-keydown="selected=false"/><br/>\
-			<div class="items" ng-hide="!text.length || selected">\
+	template : '<input type="text" ng-model="model.title" placeholder="{{prompt}}" ng-keydown="selected=false"/><br/>\
+			<div class="items" ng-hide="!model.title.length || selected">\
 				<div class="item" ng-repeat="item in items track by $index" ng-click="handleSelection(item)" style="cursor:pointer" ng-class="{active:isCurrent($index)}" ng-mouseenter="setCurrent($index)">\
 				    <img class="image" src="{{item[img]}}" ng-show="item[img]"></img>\
 				<p class="title">{{item[title]}}</p>\
