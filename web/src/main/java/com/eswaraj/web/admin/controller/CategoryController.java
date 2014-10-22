9c773d7fe59a7bcfd961405509ecf327390dac97
Ravi Sharma <ravisharma@yahoo-inc.com>
2014-10-22 11:16:21 +0100

    Added Categories Aja api

--- web/src/main/java/com/eswaraj/web/admin/controller/CategoryController.java
+++ web/src/main/java/com/eswaraj/web/admin/controller/CategoryController.java
@@ -1,19 +1,14 @@
 package com.eswaraj.web.admin.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.eswaraj.core.exceptions.ApplicationException;
-import com.eswaraj.core.service.AppService;
-import com.eswaraj.queue.service.QueueService;
-import com.eswaraj.web.dto.CategoryWithChildCategoryDto;
 
 /**
  * @author ravi
@@ -23,19 +18,13 @@ import com.eswaraj.web.dto.CategoryWithChildCategoryDto;
 @Controller
 public class CategoryController extends BaseController{
 
-	@Autowired
-	private AppService appService;
-	
     @Autowired
-    private QueueService queueService;
+    private ApiUtil apiUtil;
 
-    @Value("${aws_category_queue_name}")
-    private String categoryQueue;
-
-	@RequestMapping(value = "/mobile/categories", method = RequestMethod.GET)
-	public @ResponseBody List<CategoryWithChildCategoryDto> getAllCategories(ModelAndView mv) throws ApplicationException {
-		List<CategoryWithChildCategoryDto> allCategories = appService.getAllCategories();
-		return allCategories;
+    @RequestMapping(value = "/ajax/categories", method = RequestMethod.GET)
+    public @ResponseBody String getAllCategories(HttpServletRequest httpServletRequest) throws ApplicationException {
+        String categories = apiUtil.getAllCategopries(httpServletRequest);
+        return categories;
 	}
 
 }
