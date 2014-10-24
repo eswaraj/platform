892e3902434b5dc338255b5f12ba645afc42c8a4
vaibhav-sinha <vaibhavsinh@gmail.com>
2014-10-24 22:01:04 +0530

    Added comment logic

---
+++ web/src/main/webapp/js/singlecomplaint.js
@@ -0,0 +1,52 @@
+var complaintsApp = angular.module('complaintsApp', []);
+
+complaintsApp.controller('complaintsController', function ($scope, $http) {
+    //complaintId will have the id populated by server. Can be directly used here.
+    var getCount = 10;
+    var totalCount = 0;
+    $scope.commentText = "";
+    $scope.comments = [];
+    $scope.getNext = function () {
+        totalCount = totalCount + getCount;
+        var commentRequest = $http({
+            method: "GET",
+            url:'/ajax/complaint/' + complaintId + '/comments?count=' + getCount + '&order=DESC',
+            headers: {'Content-Type': 'application/json; charset=utf-8'}
+        });
+        commentRequest.success(function (resp) {
+            $scope.comments = resp;
+        });
+        commentRequest.error(function () {
+            console.error("/ajax/complaint/'" + complaintId + "'/comments?count=5 failed");
+        });
+    };
+    $scope.saveComment = function () {
+        var commentRequest = $http({
+            method: "POST",
+            url:"/ajax/complaint/leader/comment",
+            data: {
+                'complaintId' : complaintId,
+                'commentText' : $scope.commentText
+            },
+            headers: {'Content-Type': 'application/json; charset=utf-8'}
+        });
+        commentRequest.success(function (data) {
+            $scope.commentText = "";
+            $scope.comments = $scope.comments || [];
+            $scope.comments.unshift(data);
+            totalCount = totalCount + 1;
+        });
+        commentRequest.error(function () {
+            console.error('Request failed for /ajax/complaint/leader/comment');
+        });
+    };
+    $scope.getNext();
+});
+
+complaintsApp.filter('dateFormatter', function () {
+    return function (input) {
+        var date = new Date(input);
+        out = date.toString();
+        return out;
+    };
+});
