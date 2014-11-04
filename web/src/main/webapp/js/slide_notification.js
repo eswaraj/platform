db4b432a7fcefc0d6740b935bf193c6ac5409a3d
shailendra-gupta <helloshailendra.bitsgoa@gmail.com>
2014-11-04 17:45:49 +0530

    updated for slide_notification close btn

--- web/src/main/webapp/js/slide_notification.js
+++ web/src/main/webapp/js/slide_notification.js
@@ -7,12 +7,16 @@ slideNotice('Mark your location on the Map to select AC/PC and ward');
 slideNotice('Welcome ' + ward );
 }
 
+});
+
+function slideNotice (text) {
+$('#top_slide_notification').html('<h4>' + text + '<button type="button" id="notification_close_button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></h4>').delay(1500).slideDown(1500);
+}
+
+$(document).ready(function(){
+
 $( "#notification_close_button" ).on( 'click', function () {
 $('#top_slide_notification').hide();
 });
 
 });
-
-function slideNotice (text) {
-$('#top_slide_notification').html('<h4>' + text + '<button type="button" id="notification_close_button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></h4>').delay(1500).slideUp(1500);
-}
