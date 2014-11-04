48bf9aebf27d93c298efe6ed857d4b65ca05226c
shailendra-gupta <helloshailendra.bitsgoa@gmail.com>
2014-11-04 18:36:27 +0530

    updated for slide_notification close btn

--- web/src/main/webapp/js/slide_notification.js
+++ web/src/main/webapp/js/slide_notification.js
@@ -10,13 +10,5 @@ slideNotice('Welcome ' + ward );
 });
 
 function slideNotice (text) {
-$('#top_slide_notification').html('<h4>' + text + '</h4><button type="button" id="notification_close_button" style="position: absolute; top: 15%; right: 2%;" class="close"><span>&times;</span></button>').delay(1500).slideDown(1500);
+$('#top_slide_notification').html('<h4>' + text + '</h4><button type="button" id="notification_close_button" style="position: absolute; top: 15%; right: 2%;" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>').delay(1500).slideDown(1500);
 }
-
-$(document).ready(function(){
-
-$( "#notification_close_button" ).on( 'click', function () {
-$('#top_slide_notification').hide();
-});
-
-});
