920cf528957b951497ccee53e139619bc0b77f7a
Ravi Sharma <ping2ravi@gmail.com>
2014-11-21 07:24:13 +0000

    Query Fix

--- domain/src/main/java/com/eswaraj/domain/repo/PartyRepository.java
+++ domain/src/main/java/com/eswaraj/domain/repo/PartyRepository.java
@@ -1,5 +1,8 @@
 package com.eswaraj.domain.repo;
 
+import java.util.List;
+
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 
 import com.eswaraj.domain.nodes.Party;
@@ -7,5 +10,8 @@ import com.eswaraj.domain.nodes.Party;
 public interface PartyRepository extends GraphRepository<Party>{
 	
 	public Party getById(Long id);
+
+    @Query("match party where (party.__type__ = 'com.eswaraj.domain.nodes.Party' or party.__type__ = 'Party') return party order by party.name ASC ")
+    public List<Party> getAllParties();
 
 }
--- domain/src/main/java/com/eswaraj/domain/repo/PoliticalBodyTypeRepository.java
+++ domain/src/main/java/com/eswaraj/domain/repo/PoliticalBodyTypeRepository.java
@@ -1,10 +1,15 @@
 package com.eswaraj.domain.repo;
 
+import java.util.List;
+
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 
 import com.eswaraj.domain.nodes.PoliticalBodyType;
 
 public interface PoliticalBodyTypeRepository extends GraphRepository<PoliticalBodyType>{
 	
+    @Query("match politicalBodyType where (politicalBodyType.__type__ = 'com.eswaraj.domain.nodes.PoliticalBodyType' or politicalBodyType.__type__ = 'PoliticalBodyType') return politicalBodyType order by politicalBodyType.name ASC ")
+    public List<PoliticalBodyType> getAllPoliticalBodyTypes();
 
 }
