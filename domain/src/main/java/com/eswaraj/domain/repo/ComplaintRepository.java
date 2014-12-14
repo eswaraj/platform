cb1e10611aaf5bb54034597883471f7c125c1774
Ravi Sharma <ping2ravi@gmail.com>
2014-12-14 04:15:38 +0000

    Added Repo Function

--- domain/src/main/java/com/eswaraj/domain/repo/ComplaintRepository.java
+++ domain/src/main/java/com/eswaraj/domain/repo/ComplaintRepository.java
@@ -3,12 +3,14 @@ package com.eswaraj.domain.repo;
 import java.util.List;
 
 import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.conversion.EndResult;
 import org.springframework.data.neo4j.repository.GraphRepository;
 
 import com.eswaraj.domain.nodes.Category;
 import com.eswaraj.domain.nodes.Complaint;
 import com.eswaraj.domain.nodes.Location;
 import com.eswaraj.domain.nodes.Person;
+import com.eswaraj.domain.nodes.extended.ComplaintSearchResult;
 
 /**
  * Repository for Complaint node
@@ -57,5 +59,9 @@ public interface ComplaintRepository extends GraphRepository<Complaint>{
     @Query("start politicalAdmin=node({0}), category=node({1}) match category<-[:BELONGS_TO]-complaint-[:POLITICAL_SERVED_BY]->(politicalAdmin) where complaint.__type__ = 'com.eswaraj.domain.nodes.Complaint' return complaint order by complaint.dateCreated DESC "
             + "skip {2} limit {3}")
     public List<Complaint> getAllPagedComplaintsOfPoliticalAdminAndCategry(long politicalAdminId, long categoryId, long start, long end);
+
+    @Query("start politicalAdmin=node({0}) match complaint-[complaintPoliticalAdmin:POLITICAL_SERVED_BY]->(politicalAdmin) with complaint,complaintPoliticalAdmin match (complaint)-[:AT]-(location) with complaint,complaintPoliticalAdmin, location match (complaint)-[:LODGED_BY]-(person) with complaint,complaintPoliticalAdmin, location,person match (complaint)-[:PHOTOS_OF_COMPLAINT]-(photo)  where complaint.__type__ = 'com.eswaraj.domain.nodes.Complaint' or complaint.__type__ = 'Complaint' return complaint,complaintPoliticalAdmin,location,person,photo order by complaint.dateCreated DESC"
+            + "skip {1} limit {2}")
+    public EndResult<ComplaintSearchResult> searchAllPagedComplaintsOfPoliticalAdmin(long politicalAdminId, long start, long end);
 
 }
