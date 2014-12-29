89b8ab2bd476bf013c204832b4e9ff2253bc39dd
Ravi Sharma <ping2ravi@gmail.com>
2014-12-29 23:03:05 +0000

    Fixed defect where complaints were not getting attached with Both categories

--- domain/src/main/java/com/eswaraj/domain/repo/CategoryRepository.java
+++ domain/src/main/java/com/eswaraj/domain/repo/CategoryRepository.java
@@ -16,16 +16,16 @@ public interface CategoryRepository extends GraphRepository<Category>{
     @Query("START n=node(*) WHERE HAS(n.root) AND (n.__type__ = 'com.eswaraj.domain.nodes.Category' or n.__type__ = 'Category') and n.root = true RETURN n")
     public List<Category> getAllRootCategories();
 	
-    @Query("start category=node({0}) match (category)<-[:BELONGS_TO]-(childCategory) where childCategory.__type__ = 'com.eswaraj.domain.nodes.Category' return childCategory")
+    @Query("start category=node({0}) match (category)<-[:BELONGS_TO]-(childCategory) where childCategory.__type__ = 'com.eswaraj.domain.nodes.Category' or childCategory.__type__ = 'Category' return childCategory")
     public Collection<Category> findAllChildCategoryOfParentCategory(Category category);
 
     @Query("start category=node({0}) match (category)<-[:BELONGS_TO]-(childCategory) where (childCategory.__type__ = 'com.eswaraj.domain.nodes.Category' or childCategory.__type__ = 'Category') return childCategory")
     public List<Category> findAllChildCategoryOfParentCategory(Long categoryId);
 
-    @Query("start childCategory=node({0}) match (category)<-[:BELONGS_TO]-(childCategory) where category.__type__ = 'com.eswaraj.domain.nodes.Category' return category")
+    @Query("start childCategory=node({0}) match (category)<-[:BELONGS_TO]-(childCategory) where category.__type__ = 'com.eswaraj.domain.nodes.Category' or category.__type__ = 'Category' return category")
     public Category getParentCategory(Category category);
 
-    @Query("start complaint=node({0}) match (complaint)-[:BELONGS_TO]->(category) where category.__type__ = 'com.eswaraj.domain.nodes.Category' return category")
+    @Query("start complaint=node({0}) match (complaint)-[:BELONGS_TO]->(category) where category.__type__ = 'com.eswaraj.domain.nodes.Category' or category.__type__ = 'Category' return category")
     public Collection<Category> getCategoriesOfComplaints(Complaint complaint);
 
     @Query("match category where category.__type__ = 'com.eswaraj.domain.nodes.Category' or category.__type__ = 'Category' return category")
