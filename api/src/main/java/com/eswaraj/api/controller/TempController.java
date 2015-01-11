55742af13e26c3b313e4b78cb13bd729e00ee7bd
Ravi Sharma <ping2ravi@gmail.com>
2015-01-11 16:46:08 +0000

    test Service added for testing

---
+++ api/src/main/java/com/eswaraj/api/controller/TempController.java
@@ -0,0 +1,58 @@
+package com.eswaraj.api.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.eswaraj.core.exceptions.ApplicationException;
+import com.eswaraj.core.service.AppService;
+import com.eswaraj.core.service.PersonService;
+import com.eswaraj.messaging.dto.CommentSavedMessage;
+import com.eswaraj.messaging.dto.ComplaintViewedByPoliticalAdminMessage;
+import com.eswaraj.queue.service.QueueService;
+
+@Controller
+public class TempController extends BaseController {
+
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private AppService appService;
+    @Autowired
+    private QueueService queueService;
+
+    @RequestMapping(value = "/api/unknown/complaint/viewed", method = RequestMethod.GET)
+    public @ResponseBody String sendComplaintViewedMessage(HttpServletRequest httpServletRequest) throws ApplicationException {
+        Long complaintId = getLongParameter(httpServletRequest, "complaintId", 0L);
+        Long personId = getLongParameter(httpServletRequest, "personId", 0L);
+        Long politicalAdminId = getLongParameter(httpServletRequest, "politicalAdminId", 0L);
+        ComplaintViewedByPoliticalAdminMessage complaintViewedByPoliticalAdminMessage = new ComplaintViewedByPoliticalAdminMessage();
+        complaintViewedByPoliticalAdminMessage.setComplaintId(complaintId);
+        complaintViewedByPoliticalAdminMessage.setPersonId(personId);
+        complaintViewedByPoliticalAdminMessage.setPoliticalAdminId(politicalAdminId);
+
+        queueService.sendComplaintViewedByPoliticalLeaderMessage(complaintViewedByPoliticalAdminMessage);
+        return "Compliant Viewed Message Sent";
+    }
+
+    @RequestMapping(value = "/api/unknown/complaint/commented", method = RequestMethod.GET)
+    public @ResponseBody String sendComplaintCommentedMessage(HttpServletRequest httpServletRequest) throws ApplicationException {
+        Long complaintId = getLongParameter(httpServletRequest, "complaintId", 0L);
+        Long personId = getLongParameter(httpServletRequest, "personId", 0L);
+        Long politicalAdminId = getLongParameter(httpServletRequest, "politicalAdminId", 0L);
+        Long commentId = getLongParameter(httpServletRequest, "commentId", 0L);
+        CommentSavedMessage commentSavedMessage = new CommentSavedMessage();
+        commentSavedMessage.setComplaintId(complaintId);
+        commentSavedMessage.setPersonId(personId);
+        commentSavedMessage.setPoliticalAdminId(politicalAdminId);
+        commentSavedMessage.setCommentId(commentId);
+
+        queueService.sendCommentSavedMessage(commentSavedMessage);
+        return "Comment Saved Message Sent";
+    }
+
+}
