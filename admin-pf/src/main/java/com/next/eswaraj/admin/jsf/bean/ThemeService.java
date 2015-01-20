6e7b703cedc9729a73e7afcbebd26bf676bd75bf
Ravi Sharma <ping2ravi@gmail.com>
2015-01-20 07:03:36 +0000

    UI Fixes

--- admin-pf/src/main/java/com/next/eswaraj/admin/jsf/bean/ThemeService.java
+++ admin-pf/src/main/java/com/next/eswaraj/admin/jsf/bean/ThemeService.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Component;
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "application")
 public class ThemeService {
 
-    private String[] themes = { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer" };// , bluesky, bootstrap, casablanca, cupertino, cruze, dark-hive, delta, dot-luv, eggplant,
-                                                                                                          // excite-bike, flick, glass-x, home, hot-sneaks, humanity, le-frog, metroui, midnight,
-                                                                                                          // mint-choc, overcast, pepper-grinder, redmond, rocket, sam, smoothness, south-street, start,
-                                                                                                          // sunny, swanky-purse, trontastic, ui-darkness, ui-lightness, vader};
+    private String[] themes = { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer", "bluesky", "bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv",
+            "eggplant", "excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "metroui", "midnight", "mint-choc", "overcast", "pepper-grinder", "redmond", "rocket", "sam",
+            "smoothness", "south-street", "start", "sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };
 
     public String[] getThemes() {
         return themes;
