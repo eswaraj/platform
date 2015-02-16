0acac4ddebccadfe44b3f0a5d12d829f497eddee
Ravi Sharma <ping2ravi@gmail.com>
2015-02-16 13:11:25 +0000

    User Device query fix

--- domain/src/main/java/com/eswaraj/domain/repo/UserDeviceRepository.java
+++ domain/src/main/java/com/eswaraj/domain/repo/UserDeviceRepository.java
@@ -15,7 +15,7 @@ import com.eswaraj.domain.nodes.relationships.UserDevice;
  */
 public interface UserDeviceRepository extends GraphRepository<UserDevice> {
 	
-    @Query("start user=node({0}), device=node({1}) match (user)-[userDevice]-(device) return userDevice")
+    @Query("start user=node({0}), device=node({1}) match (user)-[:USER_DEVICE]-(device) return userDevice")
     UserDevice getUserDeviceRelation(User user, Device device);
 
 }
