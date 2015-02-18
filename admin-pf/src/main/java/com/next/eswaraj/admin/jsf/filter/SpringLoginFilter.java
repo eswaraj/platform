9afb6eaf385b04625fa8ba83e616f3f269cb734a
Ravi Sharma <ping2ravi@gmail.com>
2015-02-18 04:45:17 +0000

    Added sallu in admin

--- admin-pf/src/main/java/com/next/eswaraj/admin/jsf/filter/SpringLoginFilter.java
+++ admin-pf/src/main/java/com/next/eswaraj/admin/jsf/filter/SpringLoginFilter.java
@@ -48,7 +48,7 @@ public class SpringLoginFilter implements Filter {
             ((HttpServletResponse) response).sendRedirect(redirectUrl);
             return;
         }
-        if (user.getPerson().getEmail().equalsIgnoreCase("ping2ravi@yahoo.com")) {
+        if (user.getPerson().getEmail().equalsIgnoreCase("ping2ravi@yahoo.com") || user.getPerson().getEmail().equalsIgnoreCase("helloshailendra@yahoo.com")) {
             chain.doFilter(httpServletRequest, response);
             return;
         }
