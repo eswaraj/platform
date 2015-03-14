5f5b5b18019aaabf899190818c4f4e825a873244
Ravi Sharma <ping2ravi@gmail.com>
2015-03-14 23:23:41 +0000

    Added Cache function to get Category Counters for a location

--- cache/src/main/java/com/eswaraj/cache/redis/impl/CategoryCacheRedisImpl.java
+++ cache/src/main/java/com/eswaraj/cache/redis/impl/CategoryCacheRedisImpl.java
@@ -1,5 +1,6 @@
 package com.eswaraj.cache.redis.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Component;
 import com.eswaraj.cache.CategoryCache;
 import com.eswaraj.core.exceptions.ApplicationException;
 import com.eswaraj.web.dto.CategoryWithChildCategoryDto;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @Component
 public class CategoryCacheRedisImpl extends BaseCacheRedisImpl implements CategoryCache {
@@ -17,6 +21,9 @@ public class CategoryCacheRedisImpl extends BaseCacheRedisImpl implements Catego
     @Autowired
     @Qualifier("complaintStringRedisTemplate")
     private StringRedisTemplate complaintStringRedisTemplate;
+
+
+    private JsonParser jsonParser = new JsonParser();
 
     @Override
     public void refreshAllCategories() throws ApplicationException {
@@ -33,6 +40,33 @@ public class CategoryCacheRedisImpl extends BaseCacheRedisImpl implements Catego
         String allCategoriesKey = appKeyService.getAllCategoriesKey();
         String allCategories = complaintStringRedisTemplate.opsForValue().get(allCategoriesKey);
         return allCategories;
+    }
+
+    @Override
+    public JsonArray getAllCategoryStatsForLocation(Long locationId) throws ApplicationException {
+        String locationRedisKey = appKeyService.getLocationCounterKey(locationId);
+        String allCategories = getAllCategories();
+        JsonArray categoryJsonArray = jsonParser.parse(allCategories).getAsJsonArray();
+        List<Object> hashKeys = new ArrayList<>(categoryJsonArray.size());
+        JsonObject oneJsonObject;
+        Long categoryId;
+        for (int i = 0; i < categoryJsonArray.size(); i++) {
+            oneJsonObject = (JsonObject) categoryJsonArray.get(i);
+            categoryId = oneJsonObject.get("id").getAsLong();
+            hashKeys.add(appKeyService.getTotalComplaintCounterKey(appKeyService.getCategoryCounterKey(categoryId)));
+        }
+        logger.info("redisKey = " + locationRedisKey + ", hashKeys =" + hashKeys);
+        List<Object> resultList = complaintStringRedisTemplate.opsForHash().multiGet(locationRedisKey, hashKeys);
+
+        for (int i = 0; i < categoryJsonArray.size(); i++) {
+            oneJsonObject = (JsonObject) categoryJsonArray.get(i);
+            if (resultList.get(i) != null) {
+                oneJsonObject.addProperty("locationCount", resultList.get(i).toString());
+            } else {
+                oneJsonObject.addProperty("locationCount", 0);
+            }
+        }
+        return categoryJsonArray;
     }
 
 }
