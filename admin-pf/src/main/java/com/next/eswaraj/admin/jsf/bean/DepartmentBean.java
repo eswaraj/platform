7d74309ec7a2875decd98b256c1f7af8effb0b02
Ravi Sharma <ping2ravi@gmail.com>
2015-04-03 09:02:12 +0100

    Create Ctageory Tree

--- admin-pf/src/main/java/com/next/eswaraj/admin/jsf/bean/DepartmentBean.java
+++ admin-pf/src/main/java/com/next/eswaraj/admin/jsf/bean/DepartmentBean.java
@@ -100,7 +100,7 @@ public class DepartmentBean extends BaseBean {
             createMarker();
 
             loadLocations();
-
+            loadCategories();
 
         } catch (Exception e) {
             e.printStackTrace();
